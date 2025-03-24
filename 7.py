7f47cc63913dc59e63f1aa7803a9678bc706119b
ajey269 <127676194+ajey269@users.noreply.github.com>
2025-03-24 10:11:32 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -15,3 +15,28 @@ print(data.describe())
 if 'Category' in data.columns:
 print("\nUnique valuesin 'Category' column:")
 print(data['Category'].value_counts())
+
+
+
+Practical 7:
+ Write a Python program to read data from a CSV file, perform simple data analysis, and generate basic insights. (Use Pandas is a Python library)
+
+import pandas as pd
+
+df = pd.read_csv('student.csv') 
+print("Columns:", df.columns)
+
+df.columns = df.columns.str.strip()
+print(df.info())
+print(df.head())
+
+print(df.select_dtypes(include='number').describe())
+print(df.isnull().sum())
+
+print(df.select_dtypes(include='number').corr())
+
+import matplotlib.pyplot as plt
+df['Score'].hist(bins=20, color='skyblue', edgecolor='black')
+plt.title('Histogram of Score')
+plt.show()
+
